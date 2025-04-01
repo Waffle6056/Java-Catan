69d0d5fac9cea0f5384162b12eb4312edd667077
Seaturtle37 <andrewbrandonward@gmail.com>
2025-04-01 06:05:18 -0500

    Add files via upload

--- Road.java
+++ Road.java
@@ -1,9 +1,6 @@
-import java.util.BitSet;
-
 public class Road {
     Player owner;
     Road[] connects=new Road[4];
-    int connectionnumber=0;
     Building left, right;
     int creation=a;
     static int a=0;
@@ -19,11 +16,23 @@ public class Road {
         return owner;
     }
     public void connectRoads(Road e){
-        System.out.print(creation+" is connecting to "+e.creation+"  :");
-        connects[connectionnumber++]=e;
-        e.connects[e.connectionnumber++]=this;
-        System.out.println("Sucess");
-
+       // System.out.print(creation+" is connecting to "+e.creation+"  :");
+        for (int i = 0; i < 4; i++) {
+            if (connects[i]==null){
+                connects[i]=e;
+                break;
+            }
+           if(connects[i].creation == e.creation){
+             //  System.out.print("AllreadyDone");
+               return;
+           }
+        }
+        for (int i = 0; i < 4; i++) {
+            if (e.connects[i]==null){
+                e.connects[i]=this;
+                break;
+            }
+        }
+      //  System.out.println("Sucess");
     }
-
 }
